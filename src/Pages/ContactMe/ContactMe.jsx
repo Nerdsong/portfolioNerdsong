f2955c6e81b7771f63f1e9bd1ad9404181c95122
Nelson <jhoeladarmes@gmail.com>
2025-03-14 00:40:37 -0300

    reCaptcha added to form

--- src/Pages/ContactMe/ContactMe.jsx
+++ src/Pages/ContactMe/ContactMe.jsx
@@ -4,16 +4,27 @@ import Sent from '/src/assets/sent.json'
 import emailjs from '@emailjs/browser'
 import Lottie from 'react-lottie'
 import "./ContactMe.css"
+import ReCAPTCHA from 'react-google-recaptcha'
 
 
 function ContactMe() {
 
   const form = useRef();
+  const captcha = useRef();
+  const [captchaValido, setCaptchaValido] = useState(null)
+
+  const onChange = ()=>{
+    if(captcha.current.getValue()){
+      setCaptchaValido(true)
+    }
+    else{setCaptchaValido(false)}
+  }
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs
+    if(captcha.current.getValue()){
+      emailjs
       .sendForm('service_bv8mwio', 'template_fkmnzd5', form.current, 'ZjT1ZT1DUCqx88wEw',
       )
       .then(
@@ -26,6 +37,12 @@ function ContactMe() {
           console.log('FAILED...', error.text);
         },
       );
+    }
+    else{
+      setCaptchaValido(false)
+    }
+
+    
   };
 
   const [animation, setAnimation] = useState(EmailTrace); 
@@ -36,6 +53,9 @@ function ContactMe() {
     setIsSubmitted(true);
     setAnimation(Sent); 
     setLoop(false);
+
+    form.current.reset()
+    captcha.current.reset()
 
     // Revert to default animation after 3 seconds
     setTimeout(() => {
@@ -64,17 +84,22 @@ function ContactMe() {
       <div className='aboutme-content'>
         <form className= 'form' onSubmit= { sendEmail } ref ={form}>
           <label >Nombre:</label>
-          <input type="text" id="nombre" name="user_name" required /><br /><br />
+          <input type="text" id="nombre" name="user_name" className= "input-text" required /><br /><br />
 
           <label >Correo Electrónico:</label>
-          <input type="email" id="email" name="user_email" required /><br /><br />
+          <input type="email" id="email" name="user_email" className= "input-text" required /><br /><br />
 
           <label >Mensaje:</label><br />
-          <textarea id="mensaje" name="message" rows="5" required  ></textarea><br /><br />
-
+          <textarea id="mensaje" name="message" rows="5" className= "input-text" required  ></textarea><br /><br />
+          <ReCAPTCHA 
+            ref = {captcha}
+            sitekey="6LfR9_MqAAAAAESA4d7U4JDBkjpavzBHmdmg8S-W" 
+            onChange={onChange}
+          />
+          {captchaValido === false && <div> Por favor completa el captcha</div>}
           <input type="submit" value = "Enviar" className= "submit-button" disabled={isSubmitted}/>
         </form>
-        <div>
+      <div>
             <div className= "animation-container2">
               <Lottie options = {defaultOptions}/>
             </div>
